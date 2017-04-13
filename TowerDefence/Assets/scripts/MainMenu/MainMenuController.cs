cfcc0e879a4fa483d14f7a149a406cbc3e712562
AlexGurianov <aleks.guryanov@gmail.com>
2017-04-13 17:14:03 +0300

    Main Menu Version

---
+++ TowerDefence/Assets/scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject ExitConfirmationPanel;
+
+	// Use this for initialization
+	void Start () {
+        ExitConfirmationPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//panel.SetActive(false);
+	}
+
+    public void ExitButtonClicked()
+    {
+        ExitConfirmationPanel.SetActive(true);
+    }
+
+    public void QuitYesButtonClicked()
+    {
+        Application.Quit();
+    }
+
+    public void QuitNoButtonClicked()
+    {
+        ExitConfirmationPanel.SetActive(false);
+    }
+}
